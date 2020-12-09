7ce7e510049d665bafb3c2e4aec2e6a043be860c
hzeng <754267513@qq.com>
2020-12-09 11:15:15 +0800

    新增taskrun  pipline piplinerun

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # tektoncd-demo
 测试tektoncd pipeline client
-## 
+## 环境介绍
+kubernetes v1.18.2
+pipline v0.18.1
+
---
+++ pipline/pipline.go
@@ -0,0 +1,61 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
+	"github.com/tektoncd/pipeline/pkg/client/clientset/versioned"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/klog"
+	"tektoncd-demo/config"
+)
+
+func main() {
+
+	// k8s 配置文件
+	restConfig, err := config.GetRestConf()
+	if err != nil {
+		klog.Fatal(err)
+	}
+	// pipline clientset
+	tektonClient := versioned.NewForConfigOrDie(restConfig)
+	pipline := &v1beta1.Pipeline{
+		TypeMeta: v1.TypeMeta{},
+		ObjectMeta: v1.ObjectMeta{
+			Name:      "testpipline",
+			Namespace: "default",
+		},
+		Spec: v1beta1.PipelineSpec{
+			Description: "this is pipline",
+			Tasks: []v1beta1.PipelineTask{
+				{
+					Name: "pipline-ref-task",
+					TaskRef: &v1beta1.TaskRef{
+						Name: "testrun",
+					},
+				},
+			},
+		},
+	}
+	meta, err := tektonClient.TektonV1beta1().Pipelines("default").Create(context.Background(), pipline, v1.CreateOptions{})
+	if err != nil {
+		klog.Fatal(err)
+	}
+	klog.Info(meta)
+
+}
